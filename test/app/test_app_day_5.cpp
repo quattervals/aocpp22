876cd28ef5415d83e3029dda55899ddbaa717c43
Oliver With <oliverwith@bbv.ch>
2022-12-22 13:49:46 +0100

    move from vecs

--- test/app/test_app_day_5.cpp
+++ test/app/test_app_day_5.cpp
@@ -2,7 +2,10 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <vector>
-#include<iostream>
+#include <iostream>
+#include <list>
+#include <string>
+#include <deque>
 
 #include "day_5.hpp"
 
@@ -39,5 +42,39 @@ TEST_CASE("Day 5 Tests", "[day_4_from_input]") {
     };
     crane_9001(3, 1, 3, testdock);
     REQUIRE(expected_dock == testdock);
+  }
+
+  SECTION("Demonstrate C++'s bizzare move - leaves 'unspecified' vector items behind") {
+    std::deque<std::string> v_from;
+    std::deque<std::string> v_to;
+
+    std::string s{ " some characters" };
+
+    for (size_t i = 0; i < 10; i++) {
+      v_from.push_back(std::to_string(i) + s);
+      v_to.push_back(std::to_string(i + 10) + s);
+    }
+
+    for (auto& item : v_to) {
+      std::cout << "Before move: " << item.substr(item.size() - 2) << " @ " << &item << std::endl;
+    }
+
+    constexpr size_t number_items_to_move{ 2 };
+    std::move(v_from.end() - number_items_to_move, v_from.end(), std::back_inserter(v_to));
+
+    // The thing that is pointed to is removed. In case of strings, the actual characters are not copied.
+    // But the string object in the moved_from vector is empty
+
+    // To remove the unused items, we would have to
+    // v_from.erase(v_from.end() - number_items_to_move, v_from.end());
+
+    std::cout << "v_from still contains " << std::endl;
+    for (auto& item : v_from) {
+      std::cout << "After move: " << item << " @ " << &item << std::endl;
+    }
+    std::cout << "v_to now contains " << std::endl;
+    for (auto& item : v_to) {
+      std::cout << item << " @ " << &item << std::endl;
+    }
   }
 }
